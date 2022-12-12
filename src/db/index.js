be8c577334e3380ef7898fe2ee4f75be7f42872e
antran2704 <phamtrangiaan27@gmail.com>
2022-12-12 23:24:57 +0700

    up code

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 
 async function connect() {
-    try {
-        await mongoose.connect('mongodb+srv://antran:cunyeu1506@cluster0.mflf25c.mongodb.net/?retryWrites=true&w=majority');
-        console.log("connected database succesfully")
-    } catch (error) {
-        console.log(error, "connect database false")
-
-    }
+  try {
+    await mongoose.connect('mongodb+srv://antran:cunyeu1506@cluster0.mflf25c.mongodb.net/hotelApp?retryWrites=true&w=majority');
+    console.log("connected database succesfully");
+  } catch (error) {
+    console.log(error, "connect database false");
+  }
 }
 
-module.exports = {connect};
+module.exports = { connect };
