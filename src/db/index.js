c6cae9e42e7087f9d56cf89dadb5c6fa1b4fdc52
antran2704 <phamtrangiaan27@gmail.com>
2022-12-16 20:11:18 +0700

    .

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery", true);
 
 async function connect() {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    await mongoose.connect("mongodb+srv://antran:cunyeu1506@cluster0.mflf25c.mongodb.net/hotelApp?retryWrites=true&w=majority");
     console.log("connected database succesfully");
   } catch (error) {
     console.log(error, "connect database false");
